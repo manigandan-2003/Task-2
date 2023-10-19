b03876b7a5f1b753db541844b7feeb4b89a056cc
Manigandan Nachiappan <128712162+manigandan-2003@users.noreply.github.com>
2023-10-19 15:20:12 +0530

    src

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+// Components 
+import Calcuator from "./Calculator";
+
+function App() {
+  return (
+    <div className="container">
+      <Calcuator />
+    </div>
+  );
+}
+
+export default App;
